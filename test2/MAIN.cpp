f173f1ee4c4bc85d75de96ebb729345dc75c0570
Thomas Wagner <thomas@thowabu.de>
2017-12-03 16:16:16 +0100

    Erstes Hochladen...

---
+++ test2/MAIN.cpp
@@ -0,0 +1,20 @@
+/*
+ * MAIN.cpp
+ *
+ *  Created on: 03.12.2017
+ *      Author: thomas
+ */
+
+#include "MAIN.hpp"
+
+MAIN::MAIN ()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+MAIN::~MAIN ()
+{
+  // TODO Auto-generated destructor stub
+}
+
---
+++ test2/MAIN.hpp
@@ -0,0 +1,19 @@
+/*
+ * MAIN.hpp
+ *
+ *  Created on: 03.12.2017
+ *      Author: thomas
+ */
+
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
+
+class MAIN
+{
+public:
+  MAIN ();
+  virtual
+  ~MAIN ();
+};
+
+#endif /* MAIN_HPP_ */
